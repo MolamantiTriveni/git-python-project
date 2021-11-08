18b3d43f9c249a123ba9b7c8d058fd30670cf16f
triveni <molamantitriveni@gmail.com>
2021-11-08 19:15:38 +0530

    Triveni

---
+++ main.py
@@ -0,0 +1,15 @@
+"""n=eval(input("enter n:"))
+m=reverse(n)
+if m==n:
+     print(' it is a palindrome',n)
+else:
+      print("not palindrome")"""
+
+
+
+
+
+
+
+
+      
---
+++ main1.py
@@ -0,0 +1,5 @@
+ n=int(input("enter n:"))
+if(n**0.5)%1==0:
+	print(n,"is perfect square")
+else:
+	print(n,"is not perfect saquare")
